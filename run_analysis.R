d0eeb51b8a205a877883059cc08f5840c22afb73
Madhava Mahishi <mahishi@users.noreply.github.com>
2014-09-17 23:59:50 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,7 +3,7 @@
 ## run_analysis, the top level function that we call from R Console. This functions calls each of the 5 functions one after the other.
 ## Functions are named step_01(), step_02(), step_03(), step_04(), step_04() 
 
-## All data needed for this project is placed under folder called "project" in current working directory.
+## All data needed for this project is placed under folder called "data" in current working directory.
 
 ##Main function to get a tidy dataset for analysis
 run_analysis <- function()
@@ -41,12 +41,12 @@ step_01 <- function()
 	print("Starting Step_01")
 	## Load Training data
 	print ("Reading Training data ...")
-	training_data <- read.table("project/train/X_train.txt", header=FALSE)
+	training_data <- read.table("data/train/X_train.txt", header=FALSE)
 	cat("Training data dimensions are: "); print(dim(training_data));
 	
 	## Load Test data
 	print ("Reading Test data ...")
-	test_data <- read.table("project/test/X_test.txt", header=FALSE)
+	test_data <- read.table("data/test/X_test.txt", header=FALSE)
 	cat("Test data dimensions are: "); print(dim(test_data));
 	
 	## Merge data into a data frame by doing an rbind
@@ -72,7 +72,7 @@ step_02 <- function(full_data.tbl)
 	## data measurements features are available in features.txt file. There are 561 measurements. 
 	## features.txt file has two columns. Measurement row number and the name for that measurement.
 	## Read that data into a data frame.
-	features <- read.table("project/features.txt", header=FALSE)
+	features <- read.table("data/features.txt", header=FALSE)
 	
 	## Convert that data frame into a data table so that we can work more easily with data. Remove unnecessary variables.
 	features.tbl <- tbl_df(features); rm(features)
@@ -88,6 +88,7 @@ step_02 <- function(full_data.tbl)
 	mean_std_col_nums <- as.vector(features.tbl[,1])
 	
 	## Store column names in a global vector. Rename them to be cleaner names. We will use this vector in step 4 later.
+	##
 	mean_std_col_names <<- as.vector(features.tbl[,2])
 	mean_std_col_names <<- gsub("-", ".", mean_std_col_names)
 	mean_std_col_names <<- sub("[()]", "", mean_std_col_names)
@@ -95,6 +96,7 @@ step_02 <- function(full_data.tbl)
 	## From the full data set that we have, now take only the column numbers that we are interested in an store it in required_data.tbl
 	required_data.tbl <- select(full_data.tbl, mean_std_col_nums)
 	print("Only required measurements are selected from full data set. Other measurements ignored.")
+	cat("Required data dimensions are: "); print(dim(required_data.tbl));
 	print("Ending Step_02")
 	required_data.tbl
 }
@@ -104,7 +106,8 @@ step_03 <- function(required_data.tbl)
 {
 	print("Starting Step_03")
 	# Read activity labels from file
-	activity_labels.df <- read.table("project/activity_labels.txt", header=FALSE)
+	activity_labels.df <- read.table("data/activity_labels.txt", header=FALSE)
+	
 	## Convert activity labels and remove data frame variable.
 	activity_labels.tbl <- tbl_df(activity_labels.df); rm(activity_labels.df);
 	
@@ -112,9 +115,12 @@ step_03 <- function(required_data.tbl)
 	colnames(activity_labels.tbl) <- c('activity_id', 'activity_name')
 	
 	# Read training activities from file
-	training_activity.df <- read.table("project/train/y_train.txt", header=FALSE)
+	training_activity.df <- read.table("data/train/y_train.txt", header=FALSE)
+	cat("Training activity data dimensions are: "); print(dim(training_activity.df));
+	
 	# Read test activities from file
-	test_activity.df <- read.table("project/test/y_test.txt", header=FALSE)
+	test_activity.df <- read.table("data/test/y_test.txt", header=FALSE)
+	cat("Test activity data dimensions are: "); print(dim(test_activity.df));
 	
 	# Row Bind training and test activities into one data frame.
 	activity.df <- rbind(training_activity.df, test_activity.df); rm (training_activity.df); rm(test_activity.df);
@@ -130,9 +136,10 @@ step_03 <- function(required_data.tbl)
 	# Now column bind this activity Names to required data.
 	required_data_with_activity_names.tbl <- cbind(activity_name.tbl, required_data.tbl)
 	print("Descriptive activity names given to required data set")
-	print("Ending Step_03")
+	
 	#Return the data table. This table has activity name associated with each observation.
 	cat("Required data with activity names dimensions are: "); print(dim(required_data_with_activity_names.tbl));
+	print("Ending Step_03")
 	required_data_with_activity_names.tbl
 }
 
@@ -148,8 +155,10 @@ step_04 <- function(required_data_with_activity_names.tbl)
 	data_with_labels.tbl <- required_data_with_activity_names.tbl
 	colnames(data_with_labels.tbl) <- names
 	print("Descriptive variable names given to required data set")
-	print("Ending Step_04")
+	
 	## Return the data which now has the variable names/Labels.
+	cat("Dataset with descriptive variable names dimensions are: "); print(dim(data_with_labels.tbl));
+	print("Ending Step_04")
 	data_with_labels.tbl
 }
 
@@ -157,8 +166,8 @@ step_04 <- function(required_data_with_activity_names.tbl)
 step_05 <- function(data_with_labels.tbl)
 {
 	print("Starting Step_05")
-	training_subject <- read.table("project/train/subject_train.txt", header=FALSE)
-	test_subject <- read.table("project/test/subject_test.txt", header=FALSE)
+	training_subject <- read.table("data/train/subject_train.txt", header=FALSE)
+	test_subject <- read.table("data/test/subject_test.txt", header=FALSE)
 	subject <- rbind(training_subject, test_subject)
 	names <- c('subject', mean_std_col_names)
 	full_data.tbl <- cbind(subject, data_with_labels.tbl)
@@ -171,7 +180,7 @@ step_05 <- function(data_with_labels.tbl)
 	write.table(tidy_data.tbl, file="tidy.data.txt", sep=",", quote=FALSE, row.names = FALSE)
 	write.table(tidy_data.tbl, file="tidy.data.csv", sep=",", quote=FALSE, row.names = FALSE)
 	print("Tidy Data: Mean of selected columns, grouped by subject and activity name is stored in tidy.data.txt file")
-	print("Ending Step_05")
 	cat("Tidy data dimensions are: "); print(dim(tidy_data.tbl));
+	print("Ending Step_05")
 	tidy_data.tbl
 }
